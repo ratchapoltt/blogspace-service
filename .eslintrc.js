b906a59cbdfcb770139aea6ba4f3513d7147d610
ratchapoltt <ratchapoltt.tum@gmail.com>
2024-07-30 01:13:00 +0700

    chore(lint): setup configurations for fix and check formatter the JS and TS file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,54 @@
+const base = {
+  plugins: [],
+  extends: ["plugin:prettier/recommended"],
+  rules: {}
+};
+
+const baseJS = {
+  plugins: [...base.plugins],
+  extends: ["eslint:recommended", ...base.extends],
+  rules: {
+    ...base.rules
+  }
+};
+
+const baseTS = {
+  plugins: [...baseJS.plugins, "@typescript-eslint/eslint-plugin"],
+  extends: [...baseJS.extends, "plugin:@typescript-eslint/recommended"],
+  rules: {
+    ...baseJS.rules
+  }
+};
+
 module.exports = {
   root: true,
+  env: {
+    es6: true,
+    node: true
+  },
   overrides: [
+    {
+      files: ["*.js"],
+      parserOptions: {
+        ecmaVersion: 2022
+      },
+      plugins: baseJS.plugins,
+      extends: baseJS.extends,
+      rules: baseJS.rules
+    },
+    {
+      files: ["*.ts"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: "tsconfig.json",
+        ecmaVersion: 2022,
+        sourceType: "module"
+      },
+      plugins: baseTS.plugins,
+      extends: baseTS.extends,
+      rules: baseTS.rules
+    },
     {
       files: ["*.json", "*.json5", "*.jsonc"],
       parser: "jsonc-eslint-parser",
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: "always",
+  endOfLine: "crlf",
+  bracketSpacing: true,
+  embeddedLanguageFormatting: "auto",
+  printWidth: 120,
+  quoteProps: "consistent",
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "none",
+  useTabs: false
+};
